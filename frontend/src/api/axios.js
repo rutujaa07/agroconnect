4c621a949ef584455ab452bde284e5a038a68069
rutujaa07 <rutujamate07@gmail.com>
2026-04-13 20:50:26 +0530

    zcvssd

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://agroconnect-backend-i2e4.onrender.com/api",
 });
 
-// Automatically attach token to every request
 instance.interceptors.request.use((config) => {
   const user = JSON.parse(localStorage.getItem("agroUser"));
   if (user?.token) {
